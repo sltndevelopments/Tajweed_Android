f28365bf6b812134fdfa2e1eb33bc4bb8340ee5c
Histler <histlerbar@gmail.com>
2019-01-26 10:18:45 +0300

    new json version and files

--- app/src/main/java/ru/tajwid/app/content/db/DbHelper.kt
+++ app/src/main/java/ru/tajwid/app/content/db/DbHelper.kt
@@ -53,7 +53,7 @@ class DbHelper internal constructor(context: Context) {
 
     companion object {
         /*каждый раз, когда меняется book.json, нужно увеличивать версию*/
-        private const val DATABASE_VERSION = 16L
+        private const val DATABASE_VERSION = 17L
 
         fun deleteAll() {
             val realm = Realm.getDefaultInstance()
