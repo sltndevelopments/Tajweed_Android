cf7a03bd603840ea83c3f2e9f4bec8196d1f9024
Ilyas Zalyalov <zalyalovir@gmail.com>
2022-03-08 14:46:48 +0300

    Реализация черной темы

--- app/src/main/java/ru/tajwid/app/ui/fragment/SettingsFragment.kt
+++ app/src/main/java/ru/tajwid/app/ui/fragment/SettingsFragment.kt
@@ -1,12 +1,16 @@
 package ru.tajwid.app.ui.fragment
 
 import android.content.ActivityNotFoundException
+import android.content.Context
 import android.content.Intent
+import android.content.res.Configuration
 import android.net.Uri
+import android.os.Build
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.app.AppCompatDelegate
 import androidx.fragment.app.Fragment
 import kotlinx.android.synthetic.main.fragment_settings.*
 import ru.tajwid.app.BuildConfig
@@ -15,7 +19,17 @@ import ru.tajwid.app.ui.activity.AboutActivity
 import ru.tajwid.app.utils.FontUtils
 import ru.tajwid.app.utils.PreferencesHelper
 
+const val PREFS_NAME = "theme_prefs"
+const val KEY_THEME = "prefs.theme"
+const val THEME_UNDEFINED = -1
+const val THEME_LIGHT = 0
+const val THEME_DARK = 1
+const val THEME_SYSTEM = 2
+
+
 class SettingsFragment : Fragment() {
+
+    private val sharedPrefs by lazy { activity?. getSharedPreferences(PREFS_NAME, Context.MODE_PRIVATE) }
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -42,8 +56,9 @@ class SettingsFragment : Fragment() {
                 isChecked
             )
         }
-
-
+        initThemeListener()
+        initTheme()
+//        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.getDefaultNightMode())
     }
 
     private fun onSwitchCheckedChanged(isChecked: Boolean) {
@@ -105,4 +120,44 @@ class SettingsFragment : Fragment() {
         private const val MARKET_URL =
             "https://play.google.com/store/apps/details?id=${BuildConfig.APPLICATION_ID}"
     }
+
+    private fun initThemeListener(){
+        themeGroup.setOnCheckedChangeListener { _, checkedId ->
+            when (checkedId) {
+                R.id.themeLight -> setTheme(AppCompatDelegate.MODE_NIGHT_NO, THEME_LIGHT)
+                R.id.themeDark -> setTheme(AppCompatDelegate.MODE_NIGHT_YES, THEME_DARK)
+                R.id.themeSystem -> setTheme(AppCompatDelegate.MODE_NIGHT_FOLLOW_SYSTEM, THEME_SYSTEM)
+            }
+        }
+    }
+
+    private fun setTheme(themeMode: Int, prefsMode: Int) {
+        AppCompatDelegate.setDefaultNightMode(themeMode)
+        saveTheme(prefsMode)
+    }
+
+    private fun initTheme() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.P){
+            themeSystem.visibility = View.VISIBLE
+        } else {
+            themeSystem.visibility = View.GONE
+        }
+        when (getSavedTheme()) {
+            THEME_LIGHT -> themeLight.isChecked = true
+            THEME_DARK -> themeDark.isChecked = true
+            THEME_SYSTEM -> themeSystem.isChecked = true
+
+            THEME_UNDEFINED -> {
+                when (resources.configuration.uiMode.and(Configuration.UI_MODE_NIGHT_MASK)) {
+                    Configuration.UI_MODE_NIGHT_NO -> themeLight.isChecked = true
+                    Configuration.UI_MODE_NIGHT_YES -> themeDark.isChecked = true
+                    Configuration.UI_MODE_NIGHT_UNDEFINED -> themeLight.isChecked = true
+                }
+            }
+        }
+    }
+
+    private fun saveTheme(theme: Int) = sharedPrefs?.edit()?.putInt(KEY_THEME, theme)?.apply()
+
+    private fun getSavedTheme() = sharedPrefs?.getInt(KEY_THEME, THEME_UNDEFINED)
 }
